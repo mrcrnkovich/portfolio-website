f56cb886d2de51b82b928d183824eda0e064b86c
mrcrnkovich <mrcrnkovich@gmail.com>
2020-03-27 22:11:15 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -3,25 +3,49 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import foto from './foto.jpg';
 
+const about = 
+{
+  "data": [
+    "Michael is a certified public accountant, interested in web development and data engineering. Michael has experience working with Python for web development using Flask, and for data analysis using Pandas and Dash. He also has experience using SQL in Postgres and SQLite. This website was built using ReactJS.",
+    "In his spare time he enjoys guitar, coffee, and long bike rides on the beach.",
+    "I'm interested in networking and full stack engineering as well as data engineering roles."
+  ]
+};
+
 function About(props){
-  const pic = <img id="about-foto" src={foto} alt="Not Found"></img>;
-  const para = <div><p>Michael is a certified public accountant, interested in web development and data engineering. Michael
-      has experience working with Python for web development using Flask, and for data analysis using Pandas and Dash. He
-      also has experience using SQL in Postgres and SQLite. This website was built using ReactJS.
-      </p><p>In his spare time he enjoys guitar, coffee, and long bike rides on the beach.</p></div>;
+  const pic = <img id="about-foto" src={props.foto} alt="Not Found"></img>;
+  const para = props.data.map((paragraph)=>(<p>{paragraph}</p>));
 
-return (<div className="about">{pic}{para}</div>);
+  return (
+      <div>
+        <h3 id="about" className="sectionHeader">About</h3>
+        <div className="about">
+          {pic}
+          <div>
+            {para}
+          </div>
+        </div>
+      </div>
+    );
+}
 
-
+function Projects(props){
+  return (
+        <div className="row">
+          <Card data={props.links} />
+          <Card data={props.links} />
+          <Card data={props.links} />
+          <Card data={props.links} />
+        </div>);
 }
 
 function Card(props){
   return (
     <div className="card">
-      <a href={props.link}>
+      <a href={props.data.link}>
         <img className="thumbnail" src={foto} alt="missing file" />
-        <h4>{props.name}</h4>
-        <p>description</p>
+        <h4>{props.data.name}</h4>
+        <p>This is a description of the project</p>
       </a>
     </div>);
 }
@@ -60,36 +84,31 @@ function App() {
                 <ListLinkNewTab items={links} />
               </ul>);
   
-  const proLinks = [{link:"https://www.github.com/mrcrnkovich", name:"CRM"}]
+  const proLinks = {link:"https://www.github.com/mrcrnkovich", name:"CRM"};
+  const name ="about";
   return (
     <Router>
-    <div className="App">
+      <div className="App">
         <nav class="menu">
           <a id="homeLink" href="/"><h1>Michael Crnkovich</h1></a>
           {HeaderLinks}
         </nav>
         <div id="content">
-    <Switch>
-      <Route exact path="/">
-        <h3>Landing Page</h3>
-        <h3 id="projects">Projects</h3>
-        <div className="row">
-          <Card link={proLinks} />
-          <Card link={proLinks} />
-          <Card link={proLinks} />
-          <Card link={proLinks} />
+          <Switch>
+            <Route exact path="/">
+              <h3>Landing Page</h3>
+              <h3 id="projects">Projects</h3>
+              <Projects links={proLinks} />
+            </Route>
+            <Route path={"/"+name}>
+              <About foto={foto} data={about.data}/>
+            </Route>
+            <Route path="/blog">
+              <h3> Blog to go here</h3>
+            </Route>
+          </Switch>
         </div>
-      </Route>
-      <Route path="/about">
-        <h3 id="about">About</h3>
-        <About />
-      </Route>
-      <Route path="/blog">
-        <h3> Blog to go here</h3>
-      </Route>
-    </Switch>
-        </div>
-    </div>
+      </div>
     </Router>
   );
 }
